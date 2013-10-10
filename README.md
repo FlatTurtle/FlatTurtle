e60b4a5dc8e0d3877c62fbc55bf44e6ec05261a5
Yeri Tiete <yeri@tiete.be>
2013-10-10 14:33:13 +0200

    removed old site

--- README.md
+++
@@ -1,7 +0,0 @@
-# FlatTurtle.com 
-
-This repository contains all the files for the [FlatTurtle.com](http://flatturtle.com) website.
-
-## Copyright
-
-(c) 2011-2013 FlatTurtle
--- themes/site/js/ie8_fixes.js
+++
@@ -1,6 +0,0 @@
-$('document').ready(
-	function() {
-		$('#standard_turtle .button a').append('<span class="ie8_icon">〉</span>');
-		$('.to_top_link a').append('<span class="ie8_icon">〉</span>');
-	}
-);
