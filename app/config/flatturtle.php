12bd074c585828a349605fd2878398e8bc3859db
Yeri Tiete <yeri@tiete.be>
2013-10-11 10:41:26 +0200

    chmod, config, & first tests

--- app/config/flatturtle.php
+++ app/config/flatturtle.php
@@ -3,12 +3,12 @@
 return array(
 
 	// FlatTurtle id
-	'id' => '',
+	'id' => 'demo',
 
 	// Template file
 	'template' => 'template',
 
 	// Mailchimp form action
-	'mailchimp' => '',
+	'mailchimp' => 'http://flatturtle.us6.list-manage.com/subscribe/post?u=d82ed07d6d647a768b87d3e8d&id=8de65c1467',
 
 );
