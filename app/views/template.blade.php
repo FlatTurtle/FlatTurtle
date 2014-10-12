ccb295306a0d7183db48ac24ca66edde6df914ec
Yeri Tiete <yeri@flatturtle.com>
2014-10-12 23:48:18 +0200

    my.ft.com btn

---
+++ app/views/template.blade.php
@@ -0,0 +1,32 @@
+@extends('sitecore::template')
+
+@section('navigation')
+
+    <nav>
+        <div class="container">
+            @if (Config::get('sitecore::logo'))
+            <a href="{{ URL::to('/') }}"><img id="logo" src="{{ Config::get('sitecore::logo') }}"></a>
+            @else
+            <a id="brand" href="{{ URL::to('/') }}">{{ $flatturtle->title }}</a>
+            @endif
+            <ul>
+            @foreach ($blocks as $block)
+                @if ($block->title)
+                <li>
+                    <a href="#{{ $block->id }}">{{ $block->title }}</a>
+                </li>
+                @endif
+            @endforeach
+            @if (Config::get('sitecore::mailchimp'))
+                <li>
+                    <a href="#newsletter">{{ Lang::get('sitecore::newsletter.title') }}</a>
+                </li>
+            @endif
+                <li>
+                    <a href="https://my.flatturtle.com" class="btn colorful">{{ Lang::get('sitecore::reservations.title') }}</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+@show
